0880e17769c8b259ea40a0f4488355b977ea6a0e
Iaanka <rabssd61@gmail.com>
2026-07-10 13:41:48 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -15,7 +15,7 @@ const { sms } = require("./msg");
 const router = express.Router();
 const pino = require('pino');
 const fs = require('fs');
-const path = ("./autoreply.json");
+const path = ('./autoreply.json');
 if (!fs.existsSync(path)) fs.writeFileSync(path, JSON.stringify({}, null, 2);
 const mongoose = require('mongoose');
 const moment = require('moment-timezone');
